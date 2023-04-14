4be0d76920614ad73e991591bd8d346e65eec91a
Bardia30 <bardiadehbasti@gmail.com>
2023-04-14 00:14:31 -0400

    final commit

--- script/index.js
+++ script/index.js
@@ -1,11 +1,35 @@
 const heroImg = document.querySelector(".hero__img");
 
 let windowWidth = window.innerWidth;
-console.log(windowWidth);
-
 
 if (windowWidth < 1200){
     heroImg.src = "./assets/images/image-web-3-mobile.jpg"
     heroImg.style.width = "23.43745rem";
     heroImg.style.height = "18.75rem";
 }
+
+
+let hamburger = document.querySelector(".navbar__hamburger-icon");
+
+hamburger.addEventListener('click', (e)=>{
+    const menu = document.querySelector(".navbar-hamburger");
+    menu.style.display = 'flex';
+
+    const mask = document.querySelector('.page-mask');
+    mask.style.display = 'flex';
+
+    const navbarIcon = document.querySelector('.navbar-hamburger__icon');
+    navbarIcon.src = './assets/images/icon-menu-close.svg';
+})
+
+
+let hamburgerClose = document.querySelector('.navbar-hamburger__icon');
+
+
+hamburgerClose.addEventListener('click', ()=>{
+    const menu = document.querySelector(".navbar-hamburger");
+    menu.style.display = 'none';
+
+    const mask = document.querySelector('.page-mask');
+    mask.style.display = 'none';
+})
